cbffa34d263ad43420b621ef62aa0c3e6121cf7d
kataiga <alexandre.schone@epitech.eu>
2022-11-03 15:23:15 +0000

    feat(front): made a react like folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use yew::prelude::*;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::window;
+pub mod client;
+pub mod components;
+pub mod layouts;
 
 fn main() {
-    yew::start_app::<App>();
-}
-
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <div>
-            <h2 class={"heading"}>{"Hello, world!"}</h2>
-        </div>
-    }
-}
-
-#[wasm_bindgen(module = "/public/glue.js")]
-extern "c" {
-    #[wasm_bindgen(js_name = invokHello, catch)]
-    pub async fn hello(name: String) -> Result<JsValue, JsValue>;
+    yew::start_app::<client::Client>();
 }
---
+++ src/screens/landingPage.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+use crate::layouts::header::Header;
+
+#[function_component(LandingPage)]
+pub fn landingPage() -> Html {
+    html! {
+        <Header/>
+    }
+}
